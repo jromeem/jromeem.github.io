469a96a1e878d110ae7f53277254c5a6ca3f07eb
jerome <jerome.e.martinez@gmail.com>
2024-02-08 20:30:44 -0600

    reset and adding passthrough folder for ppshop images

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,6 @@ module.exports = function(eleventyConfig) {
   
     // Copy `img/` to `_site/img`
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("small/ppshop/images");
+
   };
