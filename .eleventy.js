8765f32d9ef82b290d2669f01a5c07276aa80855
jerome <jerome.e.martinez@gmail.com>
2026-01-14 01:00:26 -0600

    adding pass through

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
-module.exports = function(eleventyConfig) {
-    // Output directory: _site
-  
-    // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("small/ppshop/images");
+module.exports = function (eleventyConfig) {
+  // Output directory: _site
 
-  };
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("small/ppshop/images");
+  eleventyConfig.addPassthroughCopy("small/masks/videos");
+
+};
